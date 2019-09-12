614d7e9269a0638342adb0882cf8df120530eef1
Lee Pepper <pepperlk@gmail.com>
2019-09-12 10:59:40 -0600

    Update http.go

--- http.go
+++ http.go
@@ -14,10 +14,10 @@ func main() {
     }
 
     fmt.Fprintf(os.Stdout, "Listening on :%s\n", port)
-    hostname, _ := os.Hostname()
+    //hostname, _ := os.Hostname()
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(os.Stdout, "I'm %s\n", "Demo Host")
- 	fmt.Fprintf(w, "I'm %s\n", "Demo Host")
+ 	      fmt.Fprintf(w, "I'm %s\n", "Demo Host")
     })
 
 
